04c13f8a2da0724e2e6d5ef1da176a8fbef05682
Ahmad Jawad <hello@ahmadjawad.me>
2025-08-02 12:46:59 +0530

    wip: team management

--- server/src/api/teamManagement.ts
+++ server/src/api/teamManagement.ts
@@ -1,1 +1,35 @@
-export default {};
+export const getTeamManagementMeta = () => {
+  // mandatory filter: teamId
+
+  // [
+  //     {
+  //         All
+  //         count
+  //         statusus: [
+  //             {
+  //                 Not started
+  //                 count
+  //             }
+  //         ]
+  //     }
+  //     {
+  //         stage
+  //         Count
+  //     }
+  // ]
+
+  return {
+    // stageCount,
+  };
+};
+
+export const getCompetitionsForTeam = () => {
+  // mandatory filter: teamId
+  // optional filters: stage, status
+
+  return [
+    {
+        
+    }
+  ];
+};
