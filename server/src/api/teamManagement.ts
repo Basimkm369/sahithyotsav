217cf769bc1583a4c1407fe61e817fad5761fbd3
Ahmad Jawad <hello@ahmadjawad.me>
2025-08-02 22:40:39 +0530

    fix: query error

--- server/src/api/teamManagement.ts
+++ server/src/api/teamManagement.ts
@@ -22,7 +22,7 @@ router.get('/', async (req, res, next) => {
 
   const data = await executeQuery(
     `select count(cp.id) as count, stage,
-         MAX(tm.teamname) AS teamname
+         MAX(tm.teamname) AS 'teamName'
     from ofm_stages as st 
     left join ofm_competitions as cp 
       on cp.stageno = st.pid
@@ -32,10 +32,19 @@ router.get('/', async (req, res, next) => {
     group by stage`,
   );
 
+  let categories = kvStore.get(`categories`);
+  if (!categories) {
+    const categories = await executeQuery(
+      `select categoryno as no, categoryname as name from ofm_category`,
+    );
+    kvStore.set('categories', categories);
+  }
+
   return next(
     new AppResponse('', {
       teamName: data[0]?.teamname,
       competitionSummary: data,
+      categories,
     }),
   );
 });
@@ -65,6 +74,7 @@ router.get('/competitions', async (req, res, next) => {
     }
 
     let query = `select
+      COUNT(*) OVER () AS totalCount,
       im.itemname,
       ca.categoryname,
       st.stage,
@@ -95,14 +105,83 @@ router.get('/competitions', async (req, res, next) => {
       cp.status,
       cp.itemcode
     order by
-      im.itemname, ca.categoryname, st.stage
-    offset (${page} - 1) * ${limit} rows
+      im.itemname, ca.categoryname, st.stage`;
+
+    if (stageId) query += ` and cp.stageno = ${stageId}`;
+    if (status) query += ` and cp.status = ${status}`;
+    if (categoryId) query += ` and im.categoryno = ${categoryId}`;
+
+    query += ` offset (${page} - 1) * ${limit} rows
     fetch next ${limit} rows only;`;
 
-    // stageId, status, categoryId
-    if (stageId) query += ` and cp.stageno = ${stageId}`;
-    if (status) query += ` and status = ${status}`;
+    const data = await executeQuery(query);
+
+    return next(new AppResponse('', data));
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get('/participants', async (req, res, next) => {
+  try {
+    const {
+      categoryId,
+      eventId: eventIdEnc,
+      teamId: teamIdEnc,
+      limit = 10,
+      page = 1,
+    } = req.query;
+
+    let teamId = kvStore.get(`encId:${teamIdEnc}`);
+    if (!teamId) {
+      teamId = await decryptId(teamIdEnc as string);
+      kvStore.set(`encId:${teamIdEnc}`, teamId);
+    }
+
+    let eventId = kvStore.get(`encId:${eventIdEnc}`);
+    if (!eventId) {
+      eventId = await decryptId(eventIdEnc as string);
+      kvStore.set(`encId:${eventIdEnc}`, eventId);
+    }
+
+    let query = `select
+      COUNT(*) OVER () AS totalCount,
+      im.itemname,
+      ca.categoryname,
+      st.stage,
+      cp.status,
+      (
+        SELECT
+          pa.participant,
+          pa.chestno,
+          ai.status
+        FROM
+          ofm_assignitem AS ai
+          LEFT JOIN ofm_participant AS pa ON pa.chestno = ai.chestno
+        WHERE
+          ai.itemcode = cp.itemcode
+          and ai.teamnumber = ${teamId}
+          and pa.eventid = ${eventId}
+        FOR JSON PATH
+      ) AS participants
+    from
+      ofm_competitions as cp
+      inner join ofm_itemmaster as im on im.itemcode = cp.itemcode
+      inner join ofm_category as ca on ca.categoryno = im.categoryno
+      inner join ofm_stages as st on st.pid = cp.stageno
+    group by
+      im.itemname,
+      ca.categoryname,
+      st.stage,
+      cp.status,
+      cp.itemcode
+    order by
+      im.itemname, ca.categoryname, st.stage`;
+
     if (categoryId) query += ` and im.categoryno = ${categoryId}`;
+
+    query += ` offset (${page} - 1) * ${limit} rows
+    fetch next ${limit} rows only;`;
 
     const data = await executeQuery(query);
 
