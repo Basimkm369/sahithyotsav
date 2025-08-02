0aa82975223f3ef2897ae9ea90fd5a3118bf4440
Ahmad Jawad <hello@ahmadjawad.me>
2025-08-02 17:19:03 +0530

    client configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+export default ({ mode }: any) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd()))
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+  return defineConfig({
+    plugins: [
+      TanStackRouterVite({ autoCodeSplitting: true }),
+      react({
+        babel: {
+          // plugins: [['babel-plugin-react-compiler', {}]],
+        },
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      port: +(process.env.VITE_PORT || 3001),
+      host: true,
     },
-  },
-})
+  })
+}
